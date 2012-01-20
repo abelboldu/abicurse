c4bdcf3d1f5c6322d15ce356a09f539bdbe0ae8f
Abel Boldú <abel.boldu@gmx.com>
2012-01-20 17:24:30 +0100

    first commit

---
+++ abicurse.py
@@ -0,0 +1,21 @@
+import restful_lib
+from lxml import etree
+from lxml import objectify
+
+base_url = "http://10.60.10.240/api"
+conn = restful_lib.Connection(base_url, username="admin", password="xabiquo")
+dc = conn.request_get("/admin/datacenters")
+dc_tree = etree.fromstring(dc["body"].encode('ascii', 'replace'))
+dc_obj =  objectify.fromstring(dc["body"].encode('ascii', 'replace'))
+
+users = conn.request_get("/admin/enterprises/1/users")
+users_obj =  objectify.fromstring(users["body"].encode('ascii', 'replace'))
+
+i = users_obj.totalSize
+u = users_obj.user
+while i > 0:
+   print u.nick
+   u = u.getnext()
+   i = i - 1
+
+
